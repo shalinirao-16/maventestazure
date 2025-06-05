d1a86ef0608934d3dcd9b373c4815fdad91d285a
shalinirao-16 <shalinirao696@gmail.com>
2025-06-05 17:14:15 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/shalinionjava/App.java
@@ -0,0 +1,13 @@
+package com.shalinionjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
